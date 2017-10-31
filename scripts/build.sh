10588d96177bc65fb5270d5fa3466473f8bb8426
ekraihan <ekraihan@iupui.edu>
2017-10-31 19:35:13 -0400

    removing uneccessary files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+cd cordova;
+cordova platform add android;
+cd ..;
 
 if [ $TRAVIS_BRANCH = 'master' ]; then
-  echo "ember build --environment=production";
   sudo ember cordova:build --environment=production --platform=android;
 else
-  echo "ember build --environment=development"
   sudo ember cordova:build --environment=development --platform=android;
 fi
