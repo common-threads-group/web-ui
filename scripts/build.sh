48d1b1809b853cbac97229746f79a57cb24ccfd3
ekraihan <ekraihan@iupui.edu>
2017-10-31 00:17:19 -0400

    deleting cordova

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,8 @@
 
 if [ $TRAVIS_BRANCH = 'master' ]; then
   echo "ember build --environment=production";
-  ember ember cordova:build --environment=production;
+  ember cordova:build --environment=production;
 else
   echo "ember build --environment=development"
-  ember ember cordova:build --environment=development;
+  ember cordova:build --environment=development;
 fi
