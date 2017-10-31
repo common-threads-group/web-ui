d9f5dd16a9287bf061633f33b6d7905e16e92ab8
ekraihan <ekraihan@iupui.edu>
2017-10-31 17:31:26 -0400

    adding sudo

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,8 @@
 
 if [ $TRAVIS_BRANCH = 'master' ]; then
   echo "ember build --environment=production";
-  ember cordova:build --environment=production;
+  ember cordova:build --environment=production --platform=android;
 else
   echo "ember build --environment=development"
-  ember cordova:build --environment=development;
+  ember cordova:build --environment=development --platform=android;
 fi
