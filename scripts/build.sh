ee0c4a252f3ee0da37fdb5de49d10f8fc207fa51
ekraihan <ekraihan@iupui.edu>
2017-10-31 20:59:48 -0400

    trying osx'

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd cordova;
-cordova platform add android;
+cordova platform add ios;
 cd ..;
 
 if [ $TRAVIS_BRANCH = 'master' ]; then
-  sudo ember cordova:build --environment=production --platform=android;
+  sudo ember cordova:build --environment=production;
 else
-  sudo ember cordova:build --environment=development --platform=android;
+  sudo ember cordova:build --environment=development;
 fi
